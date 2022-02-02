74e72faf1d8d671dfc8f74a87f7e63c88567c8bd
ghorpadetrisha16 <98073016+ghorpadetrisha16@users.noreply.github.com>
2022-02-02 10:31:58 -0500

    AED Assignment 1 version 1
    Car Project using Swing technology

---
+++ src/main/java/com/mycompany/carproject/AEDCar/Cars.java
@@ -0,0 +1,13 @@
+package com.mycompany.carproject.AEDCar;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Trisha
+ */
+public class Cars{
+    public static void main(String args[]){
+        new HomePage().setVisible(true);
+}
+}
